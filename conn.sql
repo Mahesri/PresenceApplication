52f3a3814bf8231caabb394888e50dddd473d1a4
Mahesta Riyadi Putranto <61860990+mahesrii@users.noreply.github.com>
2023-05-14 20:46:25 +0700

    Add files via upload
    If you want to run this application, you should run the 'FrameUtama.java'.

---
+++ conn.sql
@@ -0,0 +1,2 @@
+SELECT * FROM mahasiswa LIMIT 100;
+ALTER TABLE mahasiswa CHANGE `absen_id` `absen_id` INT(10) NOT NULL AUTO_INCREMENT;
---
+++ index_query.sql
@@ -0,0 +1,2 @@
+SELECT * FROM mahasiswa;
+ALTER TABLE `mahasiswa` `absen_id` `absen_id` INT(10) NOT NULL AUTO AUTO_INCREMENT;
